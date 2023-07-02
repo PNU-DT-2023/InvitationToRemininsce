50fe25f1f8f7cd1b9861eae6aeb972c45c695f94
Milli <jinccc72@pusan.ac.kr>
2023-07-02 17:30:05 +0900

    [ADD] (임시) 마우스 움직이면 패닝같은 효과
    평면 이미지로 panning을 구현하긴 어려워 임시 효과

--- src/js/ending.js
+++ src/js/ending.js
@@ -1,6 +1,25 @@
 const imgContainer = document.querySelector('#image-now');
 const img = document.querySelector('#image-now img');
 
+// 확장 애니메이션
 setTimeout(() => {
     imgContainer.classList.toggle('expand-animation');
 }, 1000);
+
+// (임시)마우스 움직이면 이미지 변형
+setTimeout(() => {
+    imgContainer.addEventListener('mousemove', tiltImage);
+}, 2000);
+
+function tiltImage(event) {
+    const xAxis = (window.innerWidth / 2 - event.clientX)*0.01 ;
+    const yAxis = (window.innerHeight / 2 - event.clientY)*0.01 ;
+    img.style.transform = `translate(-${50}%, -50%) skew(${xAxis}deg, ${yAxis}deg)`;
+}
+  
+imgContainer.addEventListener('mouseout', resetTilt);
+
+// 초기화
+function resetTilt() {
+    img.style.transform = 'translate(-50%, -50%) skew(0)';
+}
