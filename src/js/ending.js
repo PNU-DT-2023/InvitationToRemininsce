f8d566b93ebb66d1c8ad5dd136bb42d84eef111b
Milli <jinccc72@pusan.ac.kr>
2023-07-02 17:14:12 +0900

    [FEAT] 완성 후 프레임확장 애니메이션 페이지 구현
    일단 다른 페이지로 제작(나중에 location.href 변경해 자동 이동하게 연결하면 됨)

---
+++ src/js/ending.js
@@ -0,0 +1,6 @@
+const imgContainer = document.querySelector('#image-now');
+const img = document.querySelector('#image-now img');
+
+setTimeout(() => {
+    imgContainer.classList.toggle('expand-animation');
+}, 1000);
