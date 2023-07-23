fa40dcec466ecc155b7e8b055f7014d66dbadc1b
Milli <jinccc72@pusan.ac.kr>
2023-07-23 21:17:32 +0900

    [ADD] 감상모드 구현
    ui 숨기고, home버튼 상단 배치

--- src/js/ending.js
+++ src/js/ending.js
@@ -1,13 +1,24 @@
 const frame = document.querySelector('.content-wrapper');
 const ui = document.querySelector('.ui-wrapper');
-const homeButton = document.getElementById("home-btn");
 
+const homeButton = document.getElementById("home-btn");
+const homeButtonSmall = document.getElementById("home-btn-s");
+const offPopupButton = document.getElementById("watching-mode-btn");
 
 frame.classList.add('contract-animation');
 setTimeout(() => {
         ui.classList.add('show-animation');
         homeButton.addEventListener('click', function() {
             location.href = './index.html';
+        })
+
+        offPopupButton.addEventListener('click', function() {
+            ui.classList.add('hide');
+            homeButtonSmall.classList.add('show');
+            ui.classList.add('show-animation');
+            homeButtonSmall.addEventListener('click', function() {
+                location.href = './index.html';
+            })
         })
 }, 3000);
 
