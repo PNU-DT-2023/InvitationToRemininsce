4d77e859015efa79b48a15fdddce75c8f92533eb
Milli <jinccc72@pusan.ac.kr>
2023-07-23 20:54:23 +0900

    [ADD] home 버튼 추가

--- src/js/ending.js
+++ src/js/ending.js
@@ -1,9 +1,14 @@
 const frame = document.querySelector('.content-wrapper');
 const ui = document.querySelector('.ui-wrapper');
+const homeButton = document.getElementById("home-btn");
+
 
 frame.classList.add('contract-animation');
 setTimeout(() => {
         ui.classList.add('show-animation');
+        homeButton.addEventListener('click', function() {
+            location.href = './index.html';
+        })
 }, 3000);
 
 
